969fded602441112d9ae38ac61543aa4fbab4a2e
tyt <tyt233233@gmail.com>
2023-07-14 09:02:11 +0800

    3 commit

--- src/main/java/com/tyt/GitTest.java
+++ src/main/java/com/tyt/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("GIT TEST");
         System.out.println("GIT TEST2");
+        System.out.println("GIT TEST3");
     }
 }
