4178c2627a651d94f0d115cfac263eb36f29ab92
tyt <tyt233233@gmail.com>
2023-07-14 13:46:34 +0800

    8 commit 不同版本管理测试

--- src/main/java/com/tyt/GitTest.java
+++ src/main/java/com/tyt/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("GIT TEST4 Branches（主线变更冲突测试）");
         System.out.println("GIT TEST4 Branches 冲突合并测试");
         System.out.println("GIT TEST5 push 远程库编辑推送测试");
+        System.out.println("GIT TEST5 push 不同远程库推送测试");
     }
 }
